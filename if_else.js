99cf0e4c31af835cf4a62c7a7fe8efb81130ad47
Chandana Shilpa <vchandanashilpa@deloitte.com>
2026-06-21 16:27:18 +0530

    Add if_else.js and if_else_gradecalc.js

---
+++ if_else.js
@@ -0,0 +1,7 @@
+let num = 7
+if (num % 2 == 0) {
+    console.log("number is even")
+}
+else {
+    console.log("number is odd")
+}
---
+++ if_else_gradecalc.js
@@ -0,0 +1,16 @@
+let marks = 70
+if (marks >= 90) {
+    console.log("A")
+
+} else if (marks >= 80 && marks < 90) {
+    console.log("B")
+
+} else if (marks >= 70 && marks < 80) {
+    console.log("C")
+
+} else if (marks >= 60) {
+    console.log("D")
+
+} else {
+    console.log("Fail")
+}
